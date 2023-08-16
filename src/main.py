fdd5fb9ff1b27798a772816818af2f1de0b6c876
IvanIsak2000 <vingving648@gmail.com>
2023-08-16 23:29:10 +0300

    added main file

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import time
+
+import discogs_client
+from config import token
+from time import sleep
+
+d = discogs_client.Client('ExampleApplication/0.1', user_token=token)
+
+res = d.search(genre='Rock')
+songs_on_page = res.page(1)
+for song in songs_on_page:
+    try:
+        year = d.release(song.id).year
+        print(f'Id: <{song.id}> Name: <{song.title}> Year: <{year}>')
+        time.sleep(0.5)
+
+    except discogs_client.exceptions.HTTPError:
+        print(f'Id: <{song.id}> Name: <{song.title}> Year: <Not found>')
+
+    except KeyboardInterrupt:
+        exit('Goodbye!')
